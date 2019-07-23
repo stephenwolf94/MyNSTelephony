0462779804d4d5b5b09aa94ad0410ea2b1a3604d
Stephen Wolf <contact@neoswit.fr>
2019-07-23 17:01:10 +0200

    fix 8

--- Helpers/SkypeOnlineHelper.cs
+++ Helpers/SkypeOnlineHelper.cs
@@ -19,15 +19,16 @@ public class SkypeOnlineHelper
 
     public async void getUserInfo(string upn)
     {
-        string apiUrl = "https://testneoswitv2v1.azurewebsites.net/api/HttpTriggerPowerShell1";
-        client.BaseAddress = new Uri(apiUrl);
+        //string apiUrl = "https://testneoswitv2v1.azurewebsites.net/api/HttpTriggerPowerShell1";
+        //client.BaseAddress = new Uri(apiUrl);
         //client.DefaultRequestHeaders.Accept.Clear();
         //client.DefaultRequestHeaders.Authorization
         //             = new AuthenticationHeaderValue("Bearer", token);
         //var response = await client.GetAsync("users/contact@neoswit.fr");
-        var response = await client.GetAsync("?upn=" + upn);
-        this.responseData = await response.Content.ReadAsStringAsync();
+        //var response = await client.GetAsync("?upn=" + upn);
+        //this.responseData = await response.Content.ReadAsStringAsync();
         //return this.responseData;
         //return "plop";
+        this.responseData = "plop";
     }
 }
