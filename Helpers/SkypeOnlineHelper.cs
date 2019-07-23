18efe78ac3f7772a68c58418448bdad3d8e9c365
Stephen Wolf <contact@neoswit.fr>
2019-07-23 10:38:11 +0200

    fix 3

--- Helpers/SkypeOnlineHelper.cs
+++ Helpers/SkypeOnlineHelper.cs
@@ -17,7 +17,7 @@ public class SkypeOnlineHelper
 
     }
 
-    public async void getUserInfo(string upn)
+    public async string getUserInfo(string upn)
     {
         string apiUrl = "https://testneoswitv2v1.azurewebsites.net/api/HttpTriggerPowerShell1";
         client.BaseAddress = new Uri(apiUrl);
