f974501a17dd092efdc433bc0d1d40f6a0cba77c
Stephen Wolf <contact@neoswit.fr>
2019-07-23 20:55:29 +0200

    test

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 
+
 namespace Microsoft.BotBuilderSamples.Bots
 {
     public class EchoBot : ActivityHandler
@@ -19,7 +20,7 @@ namespace Microsoft.BotBuilderSamples.Bots
             System.Diagnostics.Trace.WriteLine(upn);
             string myanswer = $"For user{upn}, here are the telephony details : ";
             System.Diagnostics.Trace.WriteLine(myanswer);
-            //skypeonlinehelper.getUserInfo(upn);
+            skypeonlinehelper.getUserInfo(upn);
             //myanswer += skypeonlinehelper.responseData;
             //System.Diagnostics.Trace.WriteLine(myanswer);
             await turnContext.SendActivityAsync(MessageFactory.Text(myanswer), cancellationToken);
