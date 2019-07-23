a7f509763235be23d47caac9c107850320b7a5f9
Stephen Wolf <contact@neoswit.fr>
2019-07-23 09:00:09 +0200

    FirstPush

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=nsaxateamstelephony-Web-Deploy.pubxml -p:Password=Wcxa0pjx7Bk11M2mTZwceQxWAvSv6f8EtoymuDAhe17yxSrx7JPSSFNMgFL5
+
